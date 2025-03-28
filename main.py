9b83e9ab76b1d2ae099dfd10c0f7a1d1755ca8f4
Meratakshay <apmerat@gmail.com>
2025-03-28 18:27:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -806,6 +806,7 @@ async def upload(bot: Client, m: Message):
     await m.reply_text(f"<pre><code>📥𝗘𝘅𝘁𝗿𝗮𝗰𝘁𝗲𝗱 𝗕𝘆 ➤『{raw_text3}』</code></pre>")
     await m.reply_text(f"<pre><code>『😏𝗥𝗲𝗮𝗰𝘁𝗶𝗼𝗻 𝗞𝗼𝗻 𝗗𝗲𝗴𝗮😏』</code></pre>")                 
 
-bot.run()
-if __name__ == "__main__":
+    bot.run()
+
+    if __name__ == "__main__":
     asyncio.run(main())
